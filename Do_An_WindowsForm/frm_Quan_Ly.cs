06560b41b54f3d9718f2743c5c875867aa50f3a5
Việt Nguyễn <vietxuannguyen@swpr6.onmicrosoft.com>
2024-10-19 09:59:53 +0700

    Add project files.

---
+++ Do_An_WindowsForm/frm_Quan_Ly.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraBars;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Do_An_WindowsForm
+{
+    public partial class frm_Quan_Ly : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public frm_Quan_Ly()
+        {
+            InitializeComponent();
+        }
+    }
+}
